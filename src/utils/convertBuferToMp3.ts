41d5302b2de453cf7ccaed4a2ceb5a7075502f07
Renan G Dutra <r.granatodutra@gmail.com>
2025-01-29 16:17:29 -0300

    chore: remove debug logs

--- src/utils/convertBuferToMp3.ts
+++ src/utils/convertBuferToMp3.ts
@@ -1,5 +1,6 @@
 import { spawn } from "node:child_process";
 import { Readable } from "node:stream";
+import Log from "./log";
 
 /**
  * Converts a given audio buffer to MP3 format using ffmpeg.
@@ -9,15 +10,13 @@ import { Readable } from "node:stream";
  * @throws An error if the ffmpeg process fails.
  */
 export default async function convertBufferToMp3(buffer: Buffer): Promise<Buffer> {
-    console.log("Starting conversion of buffer to MP3 format...");
-
-    return await new Promise<Buffer>((resolve, reject) => {
+       return await new Promise<Buffer>((resolve, reject) => {
         const readable = new Readable();
+        
         readable.push(buffer);
         readable.push(null);
 
         const chunks: any[] = [];
-        console.log("Spawning ffmpeg process...");
 
         const ffmpeg = spawn('ffmpeg', [
             '-i', 'pipe:0',
@@ -28,30 +27,25 @@ export default async function convertBufferToMp3(buffer: Buffer): Promise<Buffer
         ]);
 
         ffmpeg.stdout.on('data', (chunk) => {
-            console.log("Received data chunk from ffmpeg...");
             chunks.push(chunk);
-        });
-
-        ffmpeg.stderr.on('data', (data) => {
-            console.error(`ffmpeg stderr: ${data}`);
         });
 
         ffmpeg.on('close', (code) => {
             if (code === 0) {
-                console.log("ffmpeg process closed successfully.");
                 resolve(Buffer.concat(chunks));
             } else {
-                console.error(`ffmpeg process exited with code ${code}`);
+                Log.error(null, `ffmpeg process exited with code ${code}`);
+
                 reject(new Error('ffmpeg error. code: ' + code));
             }
         });
 
         ffmpeg.on('error', (err) => {
-            console.error(`Error in ffmpeg process: ${err.message}`);
+            Log.error(err, `Error in ffmpeg process: ${err.message}`);
+            
             reject(new Error(`Error in ffmpeg process: ${err.message}`));
         });
 
-        console.log("Piping buffer to ffmpeg stdin...");
         readable.pipe(ffmpeg.stdin);
     });
 }
